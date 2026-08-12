4613c2aafcb399cb72124b67b672e3c6ec410613
Guilherme Morais <gui.moraes29@gmail.com>
2026-08-12 09:54:36 -0300

    fix(mirror_sync): adiciona coluna a mais para ser lida atráves do read_excel
    - coluna "Código de barras" é adicionada em columns e em schema_overrides para ajustar verificação inválida (faltante) de eans e evitar erros futuros de faltos positivos/negativos

--- core/mirror_sync.py
+++ core/mirror_sync.py
@@ -7,7 +7,7 @@ def mirror_stub(caminho_base: str) -> pl.DataFrame:
     df_base = pl.read_excel(
         caminho_base,
         engine="calamine",
-        columns= ["Código"],
-        schema_overrides={"Código": pl.String}
+        columns= ["Código", "Código de barras"],
+        schema_overrides={"Código": pl.String, "Código de barras": pl.String}
     )
     return df_base
